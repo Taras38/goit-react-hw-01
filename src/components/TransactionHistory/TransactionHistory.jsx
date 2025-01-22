da6768f6b7813fe8a98f56c65fcb6baed85a37e5
Taras38 <chorniyfors@gmail.com>
2025-01-22 10:53:19 -0800

    complete

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,25 +1,23 @@
 import s from './TransactionHistory.module.css';
-const TransactionHistory = ({ items }) => {
-  return (
-    <table className={s.table}>
-      <thead className={s.thead}>
-        <tr>
-          <th className={s.th}>Type</th>
-          <th className={s.th}>Amount</th>
-          <th className={s.th}>Currency</th>
+const TransactionHistory = ({ items }) => (
+  <table className={s.table}>
+    <thead className={s.thead}>
+      <tr>
+        <th className={s.th}>Type</th>
+        <th className={s.th}>Amount</th>
+        <th className={s.th}>Currency</th>
+      </tr>
+    </thead>
+    <tbody key={items.id} className={s.tbody}>
+      {items.map(({ id, type, amount, currency }) => (
+        <tr className={s.tr}>
+          <td className={s.td}>{type}</td>
+          <td className={s.td}>{amount}</td>
+          <td className={s.td}>{currency}</td>
         </tr>
-      </thead>
-      {items.map(items => (
-        <tbody key={items.id} className={s.tbody}>
-          <tr className={s.tr}>
-            <td className={s.td}>{items.type}</td>
-            <td className={s.td}>{items.amount}</td>
-            <td className={s.td}>{items.currency}</td>
-          </tr>
-        </tbody>
       ))}
-    </table>
-  );
-};
+    </tbody>
+  </table>
+);
 
 export default TransactionHistory;
