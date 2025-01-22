0e9b5b138d405b40a4be556682292897962319cf
Taras38 <chorniyfors@gmail.com>
2025-01-22 12:18:20 -0800

    fix error with key

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -10,7 +10,7 @@ const TransactionHistory = ({ items }) => (
     </thead>
     <tbody className={s.tbody}>
       {items.map(({ id, type, amount, currency }) => (
-        <tr className={s.tr}  key={items.id}>
+        <tr className={s.tr}  key={id}>
           <td className={s.td}>{type}</td>
           <td className={s.td}>{amount}</td>
           <td className={s.td}>{currency}</td>
